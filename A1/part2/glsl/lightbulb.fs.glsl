19ee3f22a6538e684ffea4288dfbad1a8b5f349e
Reid Oliveira <oliveira.reid@gmail.com>
2017-10-15 18:11:46 -0700

    fix dup

--- A1/part2/glsl/lightbulb.fs.glsl
+++
@@ -1,4 +0,0 @@
-void main() {
-	// Set constant color
-	gl_FragColor = vec4(1, 1, 0, 1);
-}
--- a1/part1/glsl/lightbulb.vs.glsl
+++
@@ -1,9 +0,0 @@
-// The uniform variable is set up in the javascript code and the same for all vertices
-uniform vec3 lightPosition;
-
-void main() {
-	/* HINT: WORK WITH lightPosition HERE! */
-
-    // Multiply each vertex by the model-view matrix and the projection matrix to get final vertex position
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position + lightPosition, 1.0);
-}
